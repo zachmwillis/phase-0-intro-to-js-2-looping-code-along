f65f042a0d40c79d64db2c5f699499cde39cb3b9
Zach Willis <zachmw27@gmail.com>
2023-11-14 20:47:45 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message);
+    }
+    return messages;
+  }
+
+  function countDown(number) {
+    if (number < 0) {
+      console.log("Positive");
+      return;
+    }
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
